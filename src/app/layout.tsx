b6dbaabfc81197919167159622af8da077c1bed5
miguelramirez26 <26ramirez.m@gmail.com>
2026-08-05 14:36:06 +0200

    Fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Merriweather, Inter } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer"
+import Navbar from "./components/navbar";
+import Footer from "./components/footer"
 
 const merriweather = Merriweather({
   weight: ["400", "700"],
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${merriweather.variable} ${inter.variable}`}>
         <Navbar />
         {children}
